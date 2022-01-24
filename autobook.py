0e4839d2c6001553b442e43c6df285641fcd354e
Jiahao Yang <jiahao.yang@warwick.ac.uk>
2022-01-24 15:05:03 +0000

    Prettier

--- autobook.py
+++ autobook.py
@@ -5,35 +5,40 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from json import loads
+from datetime import datetime, timedelta
 import re
 
 
 class BookingCore:
-    def __init__(self, username, password, sports, date, time, length=2, policy=True):
+    def __init__(self, username, password, sports, date, time, pref_court="A1", length=1, policy=True):
 
         self.username = username  # user name
         self.password = password  # user password for sports hub
         self.sports = sports  # user prefer sports, not only squash/badminton is avialable
         self.date = date  # book court for the specific date
-        self.time = time  # book court for the specific time
-        self.policy = policy  # default is true (false), booking core will find next avialble court in a later(earlier)# time point
+        self.time = datetime.strptime(time, "%H:%M") # book court for the specific time
+        self.pref_court = pref_court # book specific court, default value is A1
+        self.policy = policy  # default is true (false), booking core will find next avialble court in a later(
+        # earlier)# time point
         self.num_success_book = 0   # the number of success book
         self.court_success_book = []  # information about the court being book already
         self.refresh_wait_time = 10  # the refresh freqeuncy in the monitoring page
         self.time_start = "23:58:00"  # the monitor process only start after this time
-        self.time_end = "00:10:00" # the booking core will end after this time
-        self.length = 2 # how many hours do the user want to book
+        self.time_end = "00:10:00"  # the booking core will end after this time
+        self.length = length  # the time difference between time 1 and time 2, set it as zero if you only need 1 slot
+        self.time2 = self.time + timedelta(hours=self.length)  # the second time point
+
 
     def init_booking(self):  # init booking core
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--incognito")
-        chrome_options.add_argument("--start-maximized");
-        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_argument("--incognito")  # incognition windows to provide clear cookies
+        chrome_options.add_argument("--start-maximized");  # Maximizing the windows to ensure component is not collapsed
+        chrome_options.add_experimental_option("detach", True) # Keep the windows on
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
         self.driver.get("https://sportwarwick.leisurecloud.net/Connect/mrmlogin.aspx")
-        self.procedure_monitor()
+        self._procedure_monitor()
 
-    def procedure_login(self):  # login
+    def _procedure_login(self):  # login
         self.driver.implicitly_wait(0.5)
         self.driver.find_element(By.ID, "ctl00_MainContent_InputLogin").send_keys(self.username)
         self.driver.find_element(By.ID, "ctl00_MainContent_InputPassword").send_keys(self.password)
@@ -42,33 +47,33 @@ class BookingCore:
             assert "Invalid User Name and Pass Word"
         else:
             print("Log in successfully")
-            self.procedure_monitor()
+            self._procedure_monitor()
 
-    def procedure_start(self):  #start booking
+    def _procedure_start(self):  # start booking
         self.driver.implicitly_wait(10)
         self.driver.find_element(By.ID, "ctl00_ctl11_Li1").click()
         print("Start Booking")
-        self.procedure_monitor()
+        self._procedure_monitor()
 
-    def procedure_monitor(self):
+    def _procedure_monitor(self):
         current_url = self.driver.current_url
         if current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmlogin.aspx":
-            self.procedure_login()
+            self._procedure_login()
 
         elif current_url == "https://sportwarwick.leisurecloud.net/Connect/memberHomePage.aspx":
-            self.procedure_start()
+            self._procedure_start()
 
-        elif (current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmselectActivityGroup.aspx" or 
+        elif (current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmselectActivityGroup.aspx" or
               current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmSelectActivity.aspx"):
-            self.procedure_select_sport()
+            self._procedure_select_sport()
 
         elif current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmResourceStatus.aspx":
-            self.procedure_enter_single_day()
+            self._procedure_enter_single_day()
 
         elif current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmProductStatus.aspx":
-            self.procedure_monitoring()
+            self._procedure_monitoring()
 
-    def check_date(self):
+    def _check_data(self):
         current_date = self.driver.find_element(By.ID, "ctl00_MainContent_lblCurrentNavDate").text
         if current_date.find(self.date) != -1:
             print(current_date)
@@ -78,30 +83,46 @@ class BookingCore:
             self.driver.find_element(By.ID,"ctl00_MainContent_Button2").click()
             return False
 
-    def procedure_monitoring(self):
-        self.procedure_locate_date()
-        available_timeslot = self.driver.find_elements(By.TAG_NAME, "input")
-        print(available_timeslot)
+    def _procedure_monitoring(self):
+        self._procedure_locate_date()
+
+        #Check length
+
+
+        available_timeslot = self.driver.find_elements(By.CLASS_NAME, "itemavailable")
+
+
         if len(available_timeslot)>0:
             print("Timeslot available")
-            for items in available_timeslot:
-                if items.get_attribute("value") == self.time:
-                    items.click()
-                    self.procedure_monitor()
-                    break
+            valid_time_slot = []
+            valid_court_info = []
+            for items in self.driver.find_elements(By.TAG_NAME,"input"):
+                if items.get_attribute("value") == datetime.strftime(self.time,"%H:%M"):
+                    valid_time_slot.append(items)
+                    court_info = list()
+                    court_info.append(re.findall('(\d+:\d+)',items.get_attribute("data-qa-id"))[0])
+                    court_info.append(re.findall('(Zone\s\w)', items.get_attribute("data-qa-id"))[0][-1])
+                    court_info.append(re.findall('(Court\s\d)', items.get_attribute("data-qa-id"))[0][-1])
+                    valid_court_info.append(court_info)
+                    print(court_info)
 
-            # insert backup logic
-            print("Can't find preferred time")
-            sleep(self.refresh_wait_time)
-            self.driver.refresh()
-            self.procedure_monitor()
+            if len(valid_time_slot) >0:
+                print("court avialble for booking")
+                print(valid_court_info)
+            else:
+                # insert backup logic
+                print("Can't find preferred time")
+                print("initiate back up logic")
+                #sleep(self.refresh_wait_time)
+                #self.driver.refresh()
+                #self._procedure_monitor()
         else:
             print("no available time slot")
             sleep(self.refresh_wait_time)
             self.driver.refresh()
-            self.procedure_monitor()
+            self._procedure_monitor()
 
-    def procedure_select_sport(self): # Start to select sport
+    def _procedure_select_sport(self): # Start to select sport
         current_url = self.driver.current_url
         if current_url == "https://sportwarwick.leisurecloud.net/Connect/mrmselectActivityGroup.aspx":
             if self.sports == "badminton":
@@ -109,7 +130,7 @@ class BookingCore:
                 for items in activity_list:
                     if items.get_attribute("value") == "Racquet Sports":
                         items.click()
-                        self.procedure_monitor()
+                        self._procedure_monitor()
                         break
         else:
             if self.sports == "badminton":
@@ -117,19 +138,19 @@ class BookingCore:
                 for items in activity_list_2:
                     if items.get_attribute("value") == "Badminton - Hub":
                         items.click()
-                        self.procedure_monitor()
+                        self._procedure_monitor()
                         break
 
-    def procedure_locate_date(self):
-        while not self.check_date():
-            self.check_date()
+    def _procedure_locate_date(self):
+        while not self._check_data():
+            self._check_data()
 
-    def procedure_enter_single_day(self):
+    def _procedure_enter_single_day(self):
         for items in self.driver.find_elements(By.TAG_NAME, "input"):
             if items.get_attribute("value") == "Available":
                 items.click()
                 sleep(2)
-                self.procedure_monitor()
+                self._procedure_monitor()
                 break
 
 
@@ -139,7 +160,7 @@ if __name__ == '__main__':
             config = loads(f.read())
 
         booking_core = BookingCore(config['username'],config["password"],config["sports"], config["date"], config["time"],
-                                   policy=True)
+                                   config['pref_court'],policy=True)
     except Exception as e:
         print(e)
         raise Exception("The booking core fail to initiate, please check the config.json")
